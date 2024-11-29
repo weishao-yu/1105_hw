93277b06ef8fa020ba2662d943a919788b61c5f2
weishao-yu <yweiwei1005@gmail.com>
2024-11-29 21:33:33 +0800

    init

---
+++ binary_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n, m, a, i = 1;
+    int result = 0;
+    int position = 0;
+    scanf("%d", &a);
+    while (a > 0)
+    {
+        n = a % 2;
+        if (position % 2 == 0)
+        {
+            n = 0;
+        }
+
+        m = n * i;
+        result = result + m;
+        i = i * 10;
+        a = a / 2;
+        position++;
+    }
+    printf("%d", result);
+    return 0;
+}
---
+++ odd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int m;
+    scanf("%d", &m);
+    if (m % 2 == 0)
+    {
+        printf("'m' is an even number\n");
+    }
+    else
+    {
+        printf("'m' is an odd number\n");
+    }
+    return 0;
+}
