4873e38ec8cb01e22c8066a128999c4da5880fa7
weishao-yu <yweiwei1005@gmail.com>
2024-12-01 10:10:19 +0800

    fix

--- binary_not.c
+++ binary_not.c
@@ -3,23 +3,31 @@ int main()
 {
     int n, m, a, b, i = 1;
     int result = 0;
+    int position = 0;
     scanf("%d", &a);
+    if (a == 0)
+    {
+        printf("1");
+        return 0;
+    }
+
     while (a > 0)
     {
         n = a % 2;
-        if (n == 0)
-        {
-            n = 1;
-        }
-        else
-        {
-            n = 0;
-        }
-
+        if (position % 2 == 0)
+            if (n == 0)
+            {
+                n = 1;
+            }
+            else
+            {
+                n = 0;
+            }
         m = n * i;
         result = result + m;
         i = i * 10;
         a = a / 2;
+        position++;
     }
     printf("%d", result);
     return 0;
